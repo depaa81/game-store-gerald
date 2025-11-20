a1bca7d406537bab69fb31ae90be9af78695dee2
depaa81 <xiyennnyennn@gmail.com>
2025-11-20 18:52:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const TELEGRAM_BOT_TOKEN = "6950291703:AAHKeH8t8XlYoIjHR8XL_33oUOejTQyHkDs";
+const TELEGRAM_CHAT_ID = "5800113255";
+
+const games = [
+    { 
+        name: "Item/Ikan fish it", 
+        price: 25.000, 
+        stock: 12, 
+        img:"https://files.catbox.moe/1wui0o.jpg",
+        spec:"https://i.imgur.com/eN5c5q4.jpeg"
+    },
+    { 
+        name: "Akun Roblox", 
+        price: 100.000, 
+        stock: 1, 
+        img:"https://files.catbox.moe/xpjea7.jpg",
+        spec:"https://i.imgur.com/iYg4y7x.jpeg"
+    },
+    { 
+        name: "Roblox Keren", 
+        price: 150.000, 
+        stock: 1, 
+        img:"https://files.catbox.moe/xpjea7.jpg",
+        spec:"https://i.imgur.com/VNkscQk.jpeg"
+    }
+];
+
+const gameList = document.getElementById("gameList");
+
+games.forEach(g=>{
+    let card = document.createElement("div");
+    card.className="game-card";
+    card.innerHTML = `
+        <img src="${g.img}" class="game-img">
+        <h2>${g.name}</h2>
+        <p>Harga: Rp ${g.price.toLocaleString()}</p>
+        <p>Stok: ${g.stock}</p>
+        <a href="${g.spec}" target="_blank" class="spec-link">📄 Lihat Spek Akun</a>
+        <button onclick="buy('${g.name}')" class="buy-btn">Beli</button>
+    `;
+    gameList.appendChild(card);
+});
+
+function buy(game){
+    document.getElementById("paymentSection").classList.remove("hidden");
+    let id = "ORD" + Math.random().toString(36).substring(2,12).toUpperCase();
+    document.getElementById("orderId").innerText = id;
+}
+
+function copyOrderId(){
+    let id = document.getElementById("orderId").innerText;
+    navigator.clipboard.writeText(id);
+    const alert = document.getElementById("copiedAlert");
+    alert.style.display="block";
+    setTimeout(()=>alert.style.display="none",1500);
+}
