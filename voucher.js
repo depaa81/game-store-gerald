526e422d4c9508c8879c3ec71fe8d01bd3df034f
depaa81 <xiyennnyennn@gmail.com>
2025-11-30 16:58:03 +0700

    Update voucher.js

--- voucher.js
+++ voucher.js
@@ -2,5 +2,5 @@
 window.VOUCHERS = [
   { code: "DISKON03", cut: 0.03, min: 0 },
   { code: "MEGA05", cut: 0.05, min: 35000 },
-  { code: "SUPER07", cut: 0.07, min: 60000 },
+  { code: "SUPER08", cut: 0.08, min: 60000 },
 ];
