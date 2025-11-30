ee3f9f76146c89b158bde032a05dbb84275c46fb
depaa81 <xiyennnyennn@gmail.com>
2025-11-30 19:49:38 +0700

    Update voucher.js

--- voucher.js
+++ voucher.js
@@ -1,6 +1,24 @@
-// Daftar voucher promo
-window.VOUCHERS = [
-  { code: "DISKON02", cut: 0.02, min: 0 },
-  { code: "MEGA04", cut: 0.04, min: 35000 },
-  { code: "SUPER06", cut: 0.06, min: 60000 },
+// VOUCHERS.JS FINAL
+const VOUCHERS = [
+  {
+    code: "DISKON02",
+    cut: 0.02,        // 2%
+    min: 0,
+    allowedProducts: "ALL" 
+    //berlaku semua produk 
+  },
+  {
+    code: "MEGA04",
+    cut: 0.04,        // 4%
+    min: 35000,
+    allowedProducts: [2]
+    // berlaku dengan produk id 2
+  },
+  {
+    code: "SUPER06",
+    cut: 0.06,        // 6%
+    min: 0,
+    allowedProducts: [4]
+    // belaku dengan produk id 4
+  }
 ];
